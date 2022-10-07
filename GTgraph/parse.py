d5c13afd17d7ca6b80fa0c81e69beaac678a641e
Aaron Orenstein <aaoren210@gmail.com>
2022-10-07 13:56:21 -0400

    Added weights to adjacency matrix

--- GTgraph/parse.py
+++ GTgraph/parse.py
@@ -8,8 +8,9 @@ def get_adj_matrix(graph_file):
 		n = int(verts[idx+1:])
 		edges = [[0] * n for _ in range(n)]
 		for line in lines[8:]:
-			v1, v2 = [int(s) for s in line.split()[1:3]]
-			edges[v1-1][v2-1] = 1
+			vals = line.split()
+			v1, v2 = [int(s) for s in vals[1:3]]
+			edges[v1-1][v2-1] = int(vals[3])
 
 	return edges
 
