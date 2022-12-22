28cb7bb908331c25412a7c33e6091ce5be039b69
Aaron3154518 <aaoren210@gmail.com>
2022-12-21 17:55:23 -0700

    Cleaned up repository

---
+++ search/dfs.cpp
@@ -0,0 +1,24 @@
+#include <chrono>
+#include <random>
+
+#include "graph.h"
+#include "parse.h"
+
+int main(int argc, char* argv[]) {
+    AdjMatrix am = Graph::parse(argv[1]);
+    
+	std::vector<int> v1s = {4, 0, 8, 4, 7}, v2s = {9, 5, 1, 3, 5};
+    for (int i = 0; i < v1s.size(); i++) {
+        auto start = std::chrono::high_resolution_clock::now();
+        Solution s = Graph::dfs(am, v1s.at(i), v2s.at(i));
+        auto stop = std::chrono::high_resolution_clock::now();
+        std::cerr << "DFS:\n" << s << std::endl;
+        std::cerr << "Time Elapsed: "
+                  << std::chrono::duration_cast<std::chrono::nanoseconds>(stop -
+                                                                          start)
+                         .count()
+                  << std::endl;
+    }
+
+    return 0;
+}
